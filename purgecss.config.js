1d93f478d61e7fc4962d6538b18081bfc0fb220e
Christopher M. Pierce <contact@chris-pierce.com>
2025-07-03 00:11:34 -0700

    add missing config

---
+++ purgecss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: ["_site/**/*.html", "_site/**/*.js"],
+    css: ["_site/assets/css/*.css"],
+    output: "_site/assets/css/",
+    skippedContentGlobs: ["_site/assets/**/*.html"],
+};
+  
